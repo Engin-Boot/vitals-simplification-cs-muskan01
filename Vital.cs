abfba2436751a8a96c8358b9f486a019e34cea18
muskan01 <35635037+muskan01@users.noreply.github.com>
2020-09-08 22:52:15 +0530

    Update Vital.cs

--- Vital.cs
+++ Vital.cs
@@ -1,1 +1,51 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
 
+namespace VitalAssignment
+{
+
+    public delegate void VitalAlert(string VitalName, string message);
+    class Vital    
+    {
+        String vitalName;
+        int lowerLimit;
+        int upperLimit;
+        public Vital(String vitalName, int lowerLimit, int upperLimit)
+        {
+            this.vitalName = vitalName;
+            this.lowerLimit = lowerLimit;
+            this.upperLimit = upperLimit;
+        }
+
+        public bool isVitalOk(int vitalValue, VitalAlert alertType)
+        {
+           
+            if (vitalValue < lowerLimit)
+            {
+                VitalAlert alert;
+                alert = alertType;
+                string message = "is LOW from the required range";
+                alert.Invoke(vitalName, message);
+
+                return false;
+            }
+            else if (vitalValue > upperLimit)
+            {
+                VitalAlert alert;
+                alert = alertType;
+                string message = "is HIGH from the required range";
+                alert.Invoke(vitalName, message);
+                return false;
+            }
+            else
+            {
+                Console.WriteLine("Vital: {0} is ok", vitalName);
+                return true;
+            }
+        }
+    }
+}
